77d3d9f6113c3a61a5e84cf0df0e33b958e75087
Willian Felipe <w.felipebraz@gmail.com>
2022-04-09 19:54:00 -0300

    1,2 e 4 feitos

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,23 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
+    
     // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
+    
+    expect(circle('')).toBeUndefined();
+    expect(typeof circle(2)).toBe('object');
+    expect(Object.values(circle(2)).length).toEqual(3);
+    expect(circle()).toBeUndefined();
+    expect(circle(2).circumference).toEqual(12.56);
+    expect(circle(3).area).toEqual(28.259999999999998);
+    expect(circle(3)).toEqual({"area": 28.259999999999998, "circumference": 18.84, "radius": 3});
+
+    // vvTeste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    // vv Teste se circle retorna um objeto.
+    // vvTeste se o objeto retornado tem 3 propriedades.
+    // vvTeste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    // vvTeste se dentro do objeto retornado, a função retorna uma key com value igual a circunferência correta para um círculo de raio 2.
+    //vv Teste se dentro do objeto retornado, a função retorna uma key com value igual a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
