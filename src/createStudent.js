f0956c0b38343258f91bee80bda71c43429eac53
Willian Felipe <w.felipebraz@gmail.com>
2022-04-09 21:07:46 -0300

    fiz a 3 e a 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+
+  
+  const estudante = {
+    name: `${name}, o Lobo Solitário`,
+    feedback: () => ('Eita pessoa boa!'),
+  };
+
+  return estudante
+};
 
 module.exports = createStudent;
